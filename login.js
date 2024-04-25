64c85972c62029743020e64d8ad3c81443c211d3
mendesdafilla <dafila.mendes@alunoescolassociais.org.br>
2024-04-25 08:10:18 -0300

    edita css

---
+++ login.js
@@ -0,0 +1,25 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmpassword').value;
+ 
+    if (!username || !password || !confirmPassword) {
+        alert('Por favor, preencha todos os campos.');
+        event.preventDefault();
+ 
+ 
+}
+});
+ 
+ 
+// Verifica senhas
+function verificaSenha(){
+    let pswd        = document.getElementById("password").value;
+    let pswdconf    = document.getElementById("confirmPassword").value;
+ 
+    if (pswd == pswdconf) {
+        alert("As senhas foram digitadas corretamente");
+    }else{
+        alert("Senhas não conferem");
+    }
+}
